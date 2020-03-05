764ce66bcfc10ea11ed6d6c7cdcd291c6f1b4804
denisptuhov <42892743+denisptuhov@users.noreply.github.com>
2020-03-05 14:32:48 +0300

    refector

--- Ptuhov_Denis/Lab1/main.cpp
+++ Ptuhov_Denis/Lab1/main.cpp
@@ -1,81 +1,66 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <ctime>
+#include <time.h>
 #include <algorithm>
 
 struct sq
 {
-    size_t x;
-    size_t y;
-    size_t len;
+    int x;
+    int y;
+    int len;
 };
 
-
-void get(std::vector<int>& a, std::vector<sq>& tmp_res, std::vector<sq>& min_res, size_t n, size_t buf, long long& iter_cnt)
+void get(std::vector<std::vector<int>>& data, std::vector<sq>& tmp_res, std::vector<sq>& min_res)
 {
-    sq elem;
+    sq point;
     bool f = false;
-    for (int i = 0; i < n; ++i)
+    for (int i = 0; i < data.size(); ++i)
     {
-        for (int j = 0; j < n; ++j)
-        {
-            ++iter_cnt;
-            if (a[i * n + j] == 0)
+        for (int j = 0; j < data.size(); ++j)
+            if (data[i][j] == 0)
             {
-                elem.x = i;
-                elem.y = j;
+                point.x = i;
+                point.y = j;
                 f = true;
                 break;
             }
-        }
         if (f)
             break;
     }
-
+    
 
     if (f == false)
     {
         min_res = tmp_res;
-        iter_cnt += tmp_res.size();
         return;
     }
 
-    for (size_t tmp_len = buf; tmp_len < n; tmp_len += buf) {
-        ++iter_cnt;
-        if (elem.x + tmp_len > n || elem.y + tmp_len > n)
+    for (int tmp_len = 1; tmp_len < data.size(); ++tmp_len)
+    {
+        if (point.x + tmp_len > data.size() || point.y + tmp_len > data.size())
             return;
 
         if (tmp_res.size() + 1 >= min_res.size())
             return;
 
-
-        for (int i = elem.x; i < elem.x + tmp_len; ++i)
-            for (int j = elem.y; j < elem.y + tmp_len; ++j)
-            {
-                ++iter_cnt;
-                if (a[i * n + j] == 1)
+        for (int i = point.x; i < point.x + tmp_len; ++i)
+            for (int j = point.y; j < point.y + tmp_len; ++j)
+                if (data[i][j] == 1)
                     return;
-            }
 
-        for (int i = elem.x; i < elem.x + tmp_len; ++i)
-            for (int j = elem.y; j < elem.y + tmp_len; ++j)
-            {
-                ++iter_cnt;
-                a[i * n + j] = 1;
-            }
+        for (int i = point.x; i < point.x + tmp_len; ++i)
+            for (int j = point.y; j < point.y + tmp_len; ++j)
+                data[i][j] = 1;
 
-        elem.len = tmp_len;
-        tmp_res.push_back(elem);
+        point.len = tmp_len;
+        tmp_res.push_back(point);
 
-        get(a, tmp_res, min_res, n, buf, iter_cnt);
+        get(data, tmp_res, min_res);
 
-        for (int i = elem.x; i < elem.x + tmp_len; ++i)
-            for (int j = elem.y; j < elem.y + tmp_len; ++j)
-            {
-                ++iter_cnt;
-                a[i * n + j] = 0;
-            }
+        for (int i = point.x; i < point.x + tmp_len; ++i)
+            for (int j = point.y; j < point.y + tmp_len; ++j)
+                data[i][j] = 0;
 
         tmp_res.pop_back();
 
@@ -83,68 +68,31 @@ void get(std::vector<int>& a, std::vector<sq>& tmp_res, std::vector<sq>& min_res
 }
 
 
-int check(int n)
-{
-    size_t ans = 1;
-    for (int i = 2; i < n; ++i)
-    {
-        if (n % i == 0)
-            ans = i;
-    }
-
-    return ans;
-}
-
 int main()
 {
-    size_t n = 0;
+    int n = 0;
     std::cin >> n;
 
-    int res = check(n);
-    long long iter_cnt = 0;
+    std::vector<std::vector<int>> a(n);
+    for (int i = 0; i < n; ++i)
+    {
+        std::vector<int> tmp(n);
+        for (int j = 0; j < n; ++j)
+            tmp[j] = 0;
+        a[i] = tmp;
+    }
 
     std::vector<sq> tmp_res;
     std::vector<sq> min_res(n * 2 + 1);
-    std::vector<int> a(n*n, 0);
 
-    if (res == 1 && n != 2) {
-
-        for (size_t i = 0; i < n / 2; ++i)
-            for (size_t j = 0; j < n / 2; ++j)
-            {
-                ++iter_cnt;
-                a[i * n + j] = 1;
-            }
-
-        for (size_t i = n / 2; i < n; ++i)
-            for (size_t j = 0; j < n / 2 + 1; j++)
-            {
-                ++iter_cnt;
-                a[i * n + j] = 1;
-            }
-
-        for (size_t i = n / 2 + n % 2; i < n; ++i)
-            for (size_t j = n / 2 + 1; j < n; ++j)
-            {
-                ++iter_cnt;
-                a[i * n + j] = 1;
-            }
-
-        tmp_res.push_back({n / 2 + 1, n / 2 + 1, n / 2});
-        tmp_res.push_back({0, 0, n / 2});
-        tmp_res.push_back({n / 2, 0, n / 2 + 1});
-    }
-
-
-    auto time1 = clock();
-    get(a, tmp_res, min_res, n, res, iter_cnt);
-    auto time2 = clock();
+    auto s1 = clock();
+    get(a, tmp_res, min_res);
+    auto s2 = clock();
 
     std::cout << min_res.size() << "\n";
     for (auto i : min_res)
         std::cout << i.x << " " << i.y << " " << i.len << "\n";
 
-    std::cout << "Iter cnt: " << iter_cnt << "\n";
-    std::cout << "Time: " << (double)(time2 - time1) / CLOCKS_PER_SEC;
+    std::cout << (double)(s2 - s1) / CLOCKS_PER_SEC;
     return 0;
 }
