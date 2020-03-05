551506d2cb3adfeda10ca797864231feac8883b5
Maria Nikolaeva <42906376+Nyashechka@users.noreply.github.com>
2020-03-05 18:51:43 +0300

    Add files via upload

--- Nikolaeva_Maria/lr_3/PiAA_lr3.cpp
+++ Nikolaeva_Maria/lr_3/PiAA_lr3.cpp
@@ -2,26 +2,30 @@
 #include <iostream>
 #include <map>
 #include <queue>
-
-using namespace std;
-
-using Graph = map<char, map<char, int>>;
-bool breadthFirstSearch(Graph& graph, char start, char end, map<char, char>& path);
-void FordFulkersonAlgorithm(Graph& graph, char start, char end);
+#include <fstream>
 
 
-void FordFulkersonAlgorithm(Graph& graph, char start, char end) {
+using Graph = std::map<char, std::map<char, int>>;
+bool breadthFirstSearch(Graph& graph, char start, char end, std::map<char, char>& path, std::ofstream& oFile);
+void FordFulkersonAlgorithm(Graph& graph, char start, char end, std::ofstream& oFile);
+
+
+void FordFulkersonAlgorithm(Graph& graph, char start, char end, std::ofstream& oFile) {
 	Graph flowGraph = graph;
 	char u, v;
+	std::string currentPath;
 
-	map<char, char> path;
+	std::map<char, char> path;
 	int maxFlow = 0;
 
-	while (breadthFirstSearch(flowGraph, start, end, path)) {
+	while (breadthFirstSearch(flowGraph, start, end, path, oFile)) {
 		int delta = INT_MAX;
+		currentPath = end;
+
 		for (v = end; v != start; v = path[v]) {
 			u = path[v];
-			delta = min(delta, flowGraph[u][v]);
+			delta = std::min(delta, flowGraph[u][v]);
+			currentPath = std::string(1, u) + " -> " + currentPath;
 		}
 		for (v = end; v != start; v = path[v]) {
 			u = path[v];
@@ -29,9 +33,10 @@ void FordFulkersonAlgorithm(Graph& graph, char start, char end) {
 			flowGraph[v][u] += delta;
 		}
 		maxFlow += delta;
+		oFile << currentPath << "\n" << "flow " << maxFlow << "\n\n\n";
 	}
 
-	cout << "Network Max Flow Value: " << maxFlow << endl;
+	oFile << "Network Max Flow Value: " << maxFlow << std::endl;
 	int flow;
 	for (auto& vertex : graph) {
 		char u = vertex.first;
@@ -46,32 +51,37 @@ void FordFulkersonAlgorithm(Graph& graph, char start, char end) {
 			else {
 				flow = throughput - flowGraph[u][v];
 			}
-			cout << "Actual flow through "<< u << " - " << v << " :" << flow << endl;
+			oFile << "Actual flow through " << u << " - " << v << " :" << flow << std::endl;
 		}
 	}
 }
 
-bool breadthFirstSearch(Graph& graph, char start, char end, map<char, char>& path) {
-	queue<char> queue;
+bool breadthFirstSearch(Graph& graph, char start, char end, std::map<char, char>& path, std::ofstream& oFile) {
+	std::queue<char> queue;
 	queue.push(start);
 
-	map<char, bool> visited;
+	std::map<char, bool> visited;
 	visited[start] = true;
+
+	oFile << "----start searching for a path----\n";
 
 	while (!queue.empty()) {
 		char vertex = queue.front();
 		queue.pop();
+		oFile << "Curent vertex " << vertex << " has connections with:\n";
 
-		for (auto neighbor : graph[vertex]) {
-			char v = neighbor.first;
-			int throughput = neighbor.second;
+			for (auto neighbor : graph[vertex]) {
+				char v = neighbor.first;
+				int throughput = neighbor.second;
+				oFile << "\t" << v << "(throughput " << throughput << ")\n";
 
-			if (!(visited[v]) && throughput > 0) {
-				queue.push(v);
-				visited[v] = true;
-				path[v] = vertex;
+				if (!(visited[v]) && throughput > 0) {
+					queue.push(v);
+					visited[v] = true;
+					path[v] = vertex;
+				}
 			}
-		}
+			oFile << "\n";
 	}
 
 	return visited[end];
@@ -82,21 +92,27 @@ int main() {
 	Graph graph;
 	char start, end, u, v;
 	int throughput, vertexCount;
+	
+	std::ifstream  inFile;
+	inFile.open("D:/test.txt");
 
-	cout << "Enter the number of edges" << endl;
-	cin >> vertexCount;
-	cout << "source" << endl;
-	cin >> start;
-	cout << "stock" << endl;
-	cin >> end;
+	std::ofstream oFile;
+	oFile.open("D:/res.txt");
 
-	cout << "Enter the edges of the graph" << endl;
+	oFile << "Enter the number of edges" << std::endl;
+	inFile >> vertexCount;
+	oFile << "source" << std::endl;
+	inFile >> start;
+	oFile << "stock" << std::endl;
+	inFile >> end;
+
+	oFile << "Enter the edges of the graph" << std::endl;
 	for (int i = 0; i < vertexCount; ++i) {
-		cin >> u >> v >> throughput;
+		inFile >> u >> v >> throughput;
 		graph[u][v] = throughput;
 	}
 
-	FordFulkersonAlgorithm(graph, start, end);
+	FordFulkersonAlgorithm(graph, start, end, oFile);
 
 	return 0;
 }
