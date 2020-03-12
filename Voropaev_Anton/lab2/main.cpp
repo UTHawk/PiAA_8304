2f086d408b27799ede3a0dbf03ed40a8e1c43e96
AntonV <42892405+StiXs221@users.noreply.github.com>
2020-03-12 19:37:36 +0300

    Main has been divided into several functions; report has been fixed

--- Voropaev_Anton/lab2/main.cpp
+++ Voropaev_Anton/lab2/main.cpp
@@ -4,7 +4,7 @@
 #include <queue>
 #include <map>
 #include <string>
-
+#include <fstream>
 
 struct node {
     inline bool operator<(const node& a) {
@@ -21,51 +21,94 @@ struct node {
 };
 
 
-int main() {
+void file_input(char& start, int& quantity, std::vector<char>& multiple_ends,  std::map<char, node>& dict) {
 
-    char start;
-    int quantity = 0;
-    std::vector <char> multiple_ends;
+    std::ifstream input("C:/Users/Anton/CLionProjects/PAA_LAB2/input");
+    input >> start;
 
-    std::cin >> start;
-
-    std::cout << "Set the number of ends\n";
-    std::cin >> quantity;
-    for(int i = 0; i < quantity; ++i) {
+    input >> quantity;
+    for (int i = 0; i < quantity; ++i) {
         char tmp_end;
-        std::cin >> tmp_end;
+        input >> tmp_end;
         multiple_ends.push_back(tmp_end);
     }
-
-
-    std::map<char, node> dict;
 
     node tmp;
     char from, to;
     float len;
     std::pair<char, int> to_child;
 
-    while(std::cin >> from >> to >> len) {
-        if(len == -1) {
+    while (input >> from >> to >> len) {
+        if (len == -1) {
             break;
         }
 
-        if(dict.find(from) == dict.end()) {
+        if (dict.find(from) == dict.end()) {
             tmp.vertex = from;
             to_child = {to, len};
             tmp.destinations.push_back(to_child);
             dict[from] = tmp;
-        }
-        else {
+        } else {
             to_child = {to, len};
             dict[from].destinations.push_back(to_child);
         }
         tmp.destinations.clear();
     }
-    for(auto i : multiple_ends) {
+    for (auto i : multiple_ends) {
         dict[i] = {};
         dict[i].vertex = i;
     }
+};
+
+
+void console_input(char& start, int& quantity, std::vector<char>& multiple_ends,  std::map<char, node>& dict) {
+    std::cin >> start;
+
+    std::cout << "Set the number of ends\n";
+    std::cin >> quantity;
+    for (int i = 0; i < quantity; ++i) {
+        char tmp_end;
+        std::cin >> tmp_end;
+        multiple_ends.push_back(tmp_end);
+    }
+
+    node tmp;
+    char from, to;
+    float len;
+    std::pair<char, int> to_child;
+
+    while (std::cin >> from >> to >> len) {
+        if (len == -1) {
+            break;
+        }
+
+        if (dict.find(from) == dict.end()) {
+            tmp.vertex = from;
+            to_child = {to, len};
+            tmp.destinations.push_back(to_child);
+            dict[from] = tmp;
+        } else {
+            to_child = {to, len};
+            dict[from].destinations.push_back(to_child);
+        }
+        tmp.destinations.clear();
+    }
+    for (auto i : multiple_ends) {
+        dict[i] = {};
+        dict[i].vertex = i;
+    }
+}
+
+
+void A_star_processing(std::vector<char>& multiple_ends,  std::map<char, node>& dict, char& start, int& mode) {
+
+    std::ofstream output;
+
+    if(mode == 1) {
+        output.open("C:/Users/Anton/CLionProjects/PAA_LAB2/output");
+    }
+
+    node tmp;
 
     std::priority_queue<node, std::vector<node>, std::less<>>  priorityQueue;
 
@@ -78,21 +121,34 @@ int main() {
         tmp = priorityQueue.top();
         priorityQueue.pop();
 
-        std::cout << "Current vertex with the lowest priority : " << tmp.vertex <<
-        "\nPath to the vertex(length) : " << tmp.length <<
-        "\nPath to the vertex(path) : " << tmp.path << std::endl;
-
+        if(mode == 1) {
+            output << "Current vertex with the lowest priority : " << tmp.vertex <<
+                   "\nPath to the vertex(length) : " << tmp.length <<
+                   "\nPath to the vertex(path) : " << tmp.path << std::endl;
+        }
+        else {
+            std::cout << "Current vertex with the lowest priority : " << tmp.vertex <<
+                      "\nPath to the vertex(length) : " << tmp.length <<
+                      "\nPath to the vertex(path) : " << tmp.path << std::endl;
+        }
 
         for(int i = 0; i < multiple_ends.size(); ++i) {
 
             if (tmp.vertex == multiple_ends[i]) {
-                std::cout << "!!!END!!! Full path to the vertex " << tmp.vertex << " : " << tmp.path + multiple_ends[i] + '\n';
+                if(mode == 1) {
+                    output << "!!!END!!! Full path to the vertex " << tmp.vertex << " : "<< tmp.path + multiple_ends[i] + '\n';
+                }
+                else {
+                    std::cout << "!!!END!!! Full path to the vertex " << tmp.vertex << " : "<< tmp.path + multiple_ends[i] + '\n';
+                }
                 multiple_ends.erase(multiple_ends.begin() + i);
                 continue;
             }
         }
-
-        std::cout << "______________________________________________________\n";
+        if(mode == 2)
+            std::cout << "______________________________________________________\n";
+        else
+            output << "______________________________________________________\n";
 
         node elem_for_push;
         for(auto i : tmp.destinations) {
@@ -105,83 +161,34 @@ int main() {
             priorityQueue.push(elem_for_push);
         }
     }
+}
+
+
+int main() {
+
+    int mode = 0;
+    std::cout << "Choose how you want to input the data:\n1 - From the file\n2 - From console\n";
+    std::cin >> mode;
+    while (mode < 1 || mode > 2) {
+        std::cout << "You need to choose 1 or 2\n";
+        std::cin >> mode;
+    }
+
+    char start;
+    int quantity = 0;
+    std::vector<char> multiple_ends;
+    std::map<char, node> dict;
+    node tmp;
+
+    if (mode == 2) {
+        console_input(start, quantity, multiple_ends, dict);
+    }
+    else {
+        file_input(start, quantity, multiple_ends, dict);
+    }
+
+    A_star_processing(multiple_ends, dict, start, mode);
+
     return 0;
 }
-/*
-a
-3
-e
-f
-h
-a b 3.0
-b c 1.0
-c d 1.0
-a d 5.0
-d e 1.0
-c f 2.0
-b h 4.0
-t t -1
-
-a
-2
-l
-h
-a b 1.000000
-a f 3.000000
-b c 5.000000
-b g 3.000000
-f g 4.000000
-c d 6.000000
-d m 1.000000
-g e 4.000000
-e h 1.000000
-e n 1.000000
-n m 2.000000
-g i 5.000000
-i j 6.000000
-i k 1.000000
-j l 5.000000
-m j 3.000000
-t t -1
-
-
-a
-2
-k
-p
-a b 1.000000
-a f 3.000000
-b c 5.000000
-b g 3.000000
-f g 4.000000
-c d 6.000000
-d m 1.000000
-g e 4.000000
-e h 1.000000
-e n 1.000000
-n m 2.000000
-g i 5.000000
-i j 6.000000
-i k 1.000000
-j l 5.000000
-m j 3.000000
-j p 4.000000
-t t -1
-
-a
-2
-f
-i
-a b 0
-a c 0
-b d 0
-c e 0
-c s 0
-d f 0
-d s 0
-s i 0
-e f 0
-t t -1
-
- */
 
